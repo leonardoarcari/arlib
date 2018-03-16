582c29ea310fe44d223619b4a270d3f79189c7fd
Leonardo Arcari <lippol94@gmail.com>
2018-03-16 17:40:25 +0100

    Use clang by default compiler for build script

--- build_exploration.sh
+++ build_exploration.sh
@@ -8,6 +8,9 @@ WORKING_DIR=`pwd`
 APPLICATION_ROOT=$WORKING_DIR
 MARGOT_ROOT=$WORKING_DIR/../core2/
 
+# Build flags
+CLANG="-DCMAKE_CXX_COMPILER=clang++"
+
 # First reset margot heel if
 rm -rf $APPLICATION_ROOT/margot_heel_if/build
 rm $APPLICATION_ROOT/margot_heel_if/config/*.conf
@@ -18,13 +21,13 @@ cp $APPLICATION_ROOT/dse_apps/$CONF_NAME $APPLICATION_ROOT/margot_heel_if/config
 # Compile margot heel interface
 mkdir -p $APPLICATION_ROOT/margot_heel_if/build || exit -1
 cd $APPLICATION_ROOT/margot_heel_if/build
-cmake -DCMAKE_BUILD_TYPE=Release -DMARGOT_CONF_FILE=$CONF_NAME .. || exit -1
+cmake $CLANG -DCMAKE_BUILD_TYPE=Release -DMARGOT_CONF_FILE=$CONF_NAME .. || exit -1
 make -j4 || exit -1
 
 # Compile application itself
 mkdir -p $APPLICATION_ROOT/build || exit -1
 cd $APPLICATION_ROOT/build
-cmake -DCMAKE_BUILD_TYPE=Release .. || exit -1
+cmake $CLANG -DCMAKE_BUILD_TYPE=Release .. || exit -1
 make -j4 || exit -1
 
 # Restore working directory
