e758b1dfa4bf98c9dd0c860c49a6d89fa89c5e9b
Leonardo Arcari <lippol94@gmail.com>
2018-03-23 17:42:50 +0100

    Fix AG graph construction from original graph and k-paths

--- src/exploration/graph_utils.hpp
+++ src/exploration/graph_utils.hpp
@@ -1,49 +1,59 @@
 #ifndef GRAPH_UTILS_H
 #define GRAPH_UTILS_H
 
-#include <vector>
 #include <cassert>
+#include <vector>
+#include <algorithm>
 
 #include "../model/graph.hpp"
 
+template<typename ForwardIt>
+ForwardIt remove_self_loops(ForwardIt first, ForwardIt last) {
+  return std::remove_if(first, last, [](const Edge &e) {
+    return e.first == e.second;
+  });
+}
+
 /**
  * @brief      Builds an Alternative Graph (AG) out of a vector of Paths.
- *						 Basically, an AG is the union of several paths from source
- *						 to target.
- *	
+ *			   Basically, an AG is the union of several paths from source
+ *			   to target.
+ *
  * @param[in]  paths  A vector of s-t-paths.
  * @param      g      The original graph G.
  *
  * @return     The AG.
  */
-RoadNetwork build_AG(const std::vector<Path>& paths, RoadNetwork& g) {
-	RoadNetwork ag{};
+RoadNetwork build_AG(const std::vector<Path> &paths, RoadNetwork &g) {
+  RoadNetwork ag{};
 
-	ag.adjListInc.reserve(g.numNodes);
-	ag.adjListOut.reserve(g.numNodes);
+  ag.adjListInc = std::vector<EdgeList>(g.numNodes);
+  ag.adjListOut = std::vector<EdgeList>(g.numNodes);
 
-	std::cerr << "paths size: " << paths.size() << ", first path size = " << paths[0].edges.size() << "\n";
+  for (auto &path : paths) {
+    auto path_edges = path.getEdges();
+    // Cleaning loops coming from dijkstra algorithm (for no reason)
+    remove_self_loops(path_edges.begin(), path_edges.end());
 
-	for (auto& path : paths) {
-		for (auto& edge : path.edges) {
-			auto lnode = edge.first;
-			auto rnode = edge.second;
-			auto w = g.getEdgeWeight(lnode, rnode);
+    // for (auto &edge : path_edges) {
+    //   std::cerr << "(" << edge.first << ", " << edge.second << ") ";
+    // }
+    // std::cerr << "\n";
 
-			std::cerr << "\n\nHEYYYYYY BUILD_AG\n\n" << "lnode = " << lnode << ", rnode = " << rnode << ", w = " << w << "\n";
+    for (auto &edge : path_edges) {
+      auto lnode = edge.first;
+      auto rnode = edge.second;
+      auto w = g.getEdgeWeight(lnode, rnode);
 
-			ag.adjListOut[lnode].insert(std::make_pair(rnode, w));
-			ag.adjListInc[rnode].insert(std::make_pair(lnode, w));
+      ag.adjListOut[lnode].insert(std::make_pair(rnode, w));
+      ag.adjListInc[rnode].insert(std::make_pair(lnode, w));
 
-			ag.numNodes += 1;
-			ag.numEdges += 2;
-		}
-	}
+      ag.numNodes += 1;
+      ag.numEdges += 2;
+    }
+  }
 
-	std::cerr << "\n\nHEYYYYYY BUILD_AG\n\n" <<
-   ag.numNodes << " " << ag.numEdges << " " << ag.adjListOut.size() << "\n";
-
-	return ag;
+  return ag;
 }
 
 #endif
