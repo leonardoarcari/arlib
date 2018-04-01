a155edb619b6225a61fa65c5ac757167d1566774
Leonardo Arcari <lippol94@gmail.com>
2018-04-01 16:35:33 +0200

    Type declaration for kSPwLO graph

---
+++ include/graph_types.hpp
@@ -0,0 +1,15 @@
+#ifndef GRAPH_TYPES_H
+#define GRAPH_TYPES_H
+
+#include <boost/graph/adjacency_list.hpp>
+#include <utility>
+
+namespace kspwlo {
+using Graph = boost::adjacency_list<boost::vecS, boost::vecS,
+                                    boost::directedS, boost::no_property,
+                                    boost::property<boost::edge_weight_t, int>>;
+using Vertex = boost::graph_traits<Graph>::vertex_descriptor;
+using Edge = std::pair<int, int>;
+}
+
+#endif
