e51a1d5c8102cc274ddb66fcab5a08710a31b109
Leonardo Arcari <lippol94@gmail.com>
2018-04-03 08:22:51 +0200

    Test read_graph_from_file()

--- test/include/test_graph_utils.cpp
+++ test/include/test_graph_utils.cpp
@@ -8,38 +8,79 @@
 #include "graph_utils.hpp"
 
 #include <algorithm>
+#include <experimental/filesystem>
+#include <fstream>
+#include <string_view>
 
-TEST_CASE("Boost::Graph can be built from .gr files", "[boost::graph]") {
-  std::string graph_gr = "d\n"
-                         "7 24\n"
-                         "0 1 6 0\n"
-                         "1 0 6 0\n"
-                         "0 3 3 0\n"
-                         "3 0 3 0\n"
-                         "0 2 4 0\n"
-                         "2 0 4 0\n"
-                         "1 6 6 0\n"
-                         "6 1 6 0\n"
-                         "3 4 5 0\n"
-                         "4 3 5 0\n"
-                         "3 1 2 0\n"
-                         "1 3 2 0\n"
-                         "3 2 3 0\n"
-                         "2 3 3 0\n"
-                         "2 4 5 0\n"
-                         "4 2 5 0\n"
-                         "3 5 3 0\n"
-                         "5 3 3 0\n"
-                         "4 5 1 0\n"
-                         "5 4 1 0\n"
-                         "4 6 2 0\n"
-                         "6 4 2 0\n"
-                         "5 6 2 0\n"
-                         "6 5 2 0\n";
+// Helper functions
+template <typename InputIterator>
+void require_vertices_in_graph(InputIterator begin, InputIterator end) {
+  REQUIRE(std::find(begin, end, 0) != end);
+  REQUIRE(std::find(begin, end, 1) != end);
+  REQUIRE(std::find(begin, end, 2) != end);
+  REQUIRE(std::find(begin, end, 3) != end);
+  REQUIRE(std::find(begin, end, 4) != end);
+  REQUIRE(std::find(begin, end, 5) != end);
+  REQUIRE(std::find(begin, end, 6) != end);
+}
 
-  auto G = boost::read_graph_from_string<kspwlo::Graph>(graph_gr);
+template <typename Edge> std::vector<Edge> build_test_edges() {
+  return std::vector<Edge>{Edge{0, 1}, Edge{0, 3}, Edge{0, 2}, Edge{1, 6},
+                           Edge{3, 4}, Edge{3, 1}, Edge{3, 2}, Edge{2, 4},
+                           Edge{3, 5}, Edge{4, 5}, Edge{4, 6}, Edge{5, 6}};
+}
 
-  SECTION("Reading gr file builds a graph with same vertices") {
+std::vector<int> build_test_weights() {
+  return std::vector<int>{6, 3, 4, 6, 5, 2, 3, 5, 3, 1, 2, 2};
+}
+
+template <typename PropertyGraph, typename Edge>
+void require_correct_weights(const std::vector<Edge> &test_edges,
+                             const std::vector<int> &test_weights,
+                             const PropertyGraph &G) {
+  using namespace boost;
+  auto weights = get(edge_weight, G);
+  for (decltype(test_edges.size()) i = 0; i < test_edges.size(); ++i) {
+    // Check weights inside graph structure
+    auto[u, v] = test_edges[i];
+    auto e = edge(u, v, G);
+
+    REQUIRE(e.second);
+    REQUIRE(weights[e.first] == test_weights[i]);
+  }
+}
+
+constexpr std::string_view graph_gr = "d\n"
+                                      "7 24\n"
+                                      "0 1 6 0\n"
+                                      "1 0 6 0\n"
+                                      "0 3 3 0\n"
+                                      "3 0 3 0\n"
+                                      "0 2 4 0\n"
+                                      "2 0 4 0\n"
+                                      "1 6 6 0\n"
+                                      "6 1 6 0\n"
+                                      "3 4 5 0\n"
+                                      "4 3 5 0\n"
+                                      "3 1 2 0\n"
+                                      "1 3 2 0\n"
+                                      "3 2 3 0\n"
+                                      "2 3 3 0\n"
+                                      "2 4 5 0\n"
+                                      "4 2 5 0\n"
+                                      "3 5 3 0\n"
+                                      "5 3 3 0\n"
+                                      "4 5 1 0\n"
+                                      "5 4 1 0\n"
+                                      "4 6 2 0\n"
+                                      "6 4 2 0\n"
+                                      "5 6 2 0\n"
+                                      "6 5 2 0\n";
+
+TEST_CASE("Boost::Graph can be built from .gr strings", "[boost::graph]") {
+  auto G = boost::read_graph_from_string<kspwlo::Graph>(std::string{graph_gr});
+
+  SECTION("Reading gr string builds a graph with same vertices") {
     using namespace boost;
     using Vertex = graph_traits<kspwlo::Graph>::vertex_descriptor;
 
@@ -51,36 +92,72 @@ TEST_CASE("Boost::Graph can be built from .gr files", "[boost::graph]") {
     REQUIRE(num_vertices(G) == 7);
 
     // All the vertices are in graph structure
-    REQUIRE(std::find(std::begin(Vs), v_end, 0) != v_end);
-    REQUIRE(std::find(std::begin(Vs), v_end, 1) != v_end);
-    REQUIRE(std::find(std::begin(Vs), v_end, 2) != v_end);
-    REQUIRE(std::find(std::begin(Vs), v_end, 3) != v_end);
-    REQUIRE(std::find(std::begin(Vs), v_end, 4) != v_end);
-    REQUIRE(std::find(std::begin(Vs), v_end, 5) != v_end);
-    REQUIRE(std::find(std::begin(Vs), v_end, 6) != v_end);
+    require_vertices_in_graph(std::begin(Vs), std::end(Vs));
   }
 
-  SECTION("Reading gr file builds a graph with edges with correct weights") {
+  SECTION("Reading gr string builds a graph with edges with correct weights") {
     using namespace boost;
     using kspwlo::Edge;
 
-    auto test_edges = std::vector<Edge>{
-        Edge{0, 1}, Edge{0, 3}, Edge{0, 2}, Edge{1, 6}, Edge{3, 4}, Edge{3, 1},
-        Edge{3, 2}, Edge{2, 4}, Edge{3, 5}, Edge{4, 5}, Edge{4, 6}, Edge{5, 6}};
-    auto test_weights = std::vector<int>{6, 3, 4, 6, 5, 2, 3, 5, 3, 1, 2, 2};
+    auto test_edges = build_test_edges<Edge>();
+    auto test_weights = build_test_weights();
 
     REQUIRE(num_edges(G) == test_edges.size() * 2);
 
-    auto weights = get(edge_weight, G);
-    for (decltype(test_edges.size()) i = 0; i < test_edges.size(); ++i) {
-      // Check weights inside graph structure
-      auto[u, v] = test_edges[i];
-      auto e = edge(u, v, G);
+    require_correct_weights(test_edges, test_weights, G);
+  }
+}
 
-      std::cout << u << " " << v << "\n";
+TEST_CASE("Boost::Graph can be built from .gr files", "[boost::graph]") {
+  // Create a new tmp file out of graph_gr
+  namespace fs = std::experimental::filesystem;
+  auto path = fs::temp_directory_path() / "graph_gr_file.gr";
+  auto of = std::ofstream(path.string());
+  of << graph_gr;
+  of.close();
 
-      REQUIRE(e.second);
-      REQUIRE(weights[e.first] == test_weights[i]);
-    }
+  // Parse file
+  using namespace boost;
+  auto G_opt = read_graph_from_file<kspwlo::Graph>(path.string());
+
+  SECTION("Reading existing gr files builds a graph with same vertices") {
+    // File must exist
+    REQUIRE(G_opt);
+    auto G = *G_opt;
+
+    using Vertex = graph_traits<kspwlo::Graph>::vertex_descriptor;
+
+    auto Vs = std::vector<Vertex>{vertices(G).first, vertices(G).second};
+
+    auto v_end = std::end(Vs);
+
+    // Only 7 vertices are in the graph structure
+    REQUIRE(num_vertices(G) == 7);
+
+    // All the vertices are in graph structure
+    require_vertices_in_graph(std::begin(Vs), std::end(Vs));
+  }
+
+  SECTION("Reading existing gr file builds a graph with edges with correct weights") {
+    // File must exist
+    REQUIRE(G_opt);
+    auto G = *G_opt;
+
+    using kspwlo::Edge;
+
+    auto test_edges = build_test_edges<Edge>();
+    auto test_weights = build_test_weights();
+
+    REQUIRE(num_edges(G) == test_edges.size() * 2);
+
+    require_correct_weights(test_edges, test_weights, G);
+  }
+
+  SECTION("Reading not-existing gr file returns empty optional") {
+    // File must not exist
+    auto non_existing_path = fs::path("/xyz/bla/bla/come/on/cant/be/existing.gr");
+    G_opt = read_graph_from_file<kspwlo::Graph>(non_existing_path.string());
+
+    REQUIRE(!G_opt);
   }
 }
