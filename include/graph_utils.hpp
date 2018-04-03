1c2347cea40b632b12df8c4528ec147994386688
Leonardo Arcari <lippol94@gmail.com>
2018-04-03 18:10:49 +0200

    Fix multiple definition error for dump_edges_weight specialization

--- include/graph_utils.hpp
+++ include/graph_utils.hpp
@@ -78,18 +78,7 @@ read_graph_from_file(const std::string_view &path) {
 template <typename PropertyGraph>
 std::string dump_edges_weight(const PropertyGraph &G);
 
-template <> std::string dump_edges_weight(const kspwlo::Graph &G) {
-  auto ss = std::stringstream{};
-  auto weight = get(edge_weight, G);
-
-  for (auto ei = edges(G).first; ei != edges(G).second; ++ei) {
-    auto w = weight[*ei];
-
-    ss << "(" << source(*ei, G) << ", " << target(*ei, G) << "; " << w << ") ";
-  }
-
-  return ss.str();
-}
+template <> std::string dump_edges_weight(const kspwlo::Graph &G);
 } // namespace boost
 
 #endif
---
+++ src/graph_utils.cpp
@@ -0,0 +1,16 @@
+#include "graph_utils.hpp"
+
+namespace boost {
+template <> std::string dump_edges_weight(const kspwlo::Graph &G) {
+  auto ss = std::stringstream{};
+  auto weight = get(edge_weight, G);
+
+  for (auto ei = edges(G).first; ei != edges(G).second; ++ei) {
+    auto w = weight[*ei];
+
+    ss << "(" << source(*ei, G) << ", " << target(*ei, G) << "; " << w << ") ";
+  }
+
+  return ss.str();
+}
+} // namespace boost
