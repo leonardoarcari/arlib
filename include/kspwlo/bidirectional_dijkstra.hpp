4107c7bb4d04001bf2785cced4a718a43a345ced
Leonardo Arcari <lippol94@gmail.com>
2018-05-07 16:50:36 +0200

    First ugly implementation of Bidirectional Dijkstra for Boost::Graph

--- include/kspwlo/bidirectional_dijkstra.hpp
+++ include/kspwlo/bidirectional_dijkstra.hpp
@@ -7,6 +7,7 @@
 #include <boost/graph/reverse_graph.hpp>
 
 #include "kspwlo/impl/bidirectional_dijkstra_impl.hpp"
+#include "kspwlo/impl/kspwlo_impl.hpp"
 
 #include <limits>
 #include <vector>
@@ -17,25 +18,88 @@
 namespace boost {
 template <
     typename Graph, typename PredecessorMap, typename DistanceMap,
-    typename WeightMap, typename VertexIndexMap,
+    typename WeightMap, typename VertexIndexMap, typename BackGraph,
+    typename BackWeightMap, typename BackIndexMap,
     typename Vertex = typename boost::graph_traits<Graph>::vertex_descriptor>
 void bidirectional_dijkstra(const Graph &G, Vertex s, Vertex t,
                             PredecessorMap predecessor, DistanceMap distance,
-                            WeightMap weight, VertexIndexMap index_map) {
+                            WeightMap weight, VertexIndexMap index_map,
+                            const BackGraph &G_b, BackWeightMap weight_b,
+                            BackIndexMap index_map_b) {
   using namespace boost;
   using Length = typename property_traits<DistanceMap>::value_type;
   constexpr Length inf = std::numeric_limits<Length>::max();
 
-  // Initialize data structures
-  kspwlo_impl::init_distance_vector(distance, s, index_map);
+  // Initialize distance structures
+  kspwlo_impl::init_distance_vector(G, distance, s);
 
-  // Backwards data structures
-  auto rev_G = make_reverse_graph(G);
-  auto index_map_b = get(vertex_index, rev_G);
-  auto distance_b_vec = std::vector<Length>(num_vertices(G));
-  kspwlo_impl::init_distance_vector(distance_b_vec, t, index_map_b);
-  auto distance_b = make_iterator_property_map(std::begin(distance_b_vec), index_map_b);
-  auto weight_b = kspwlo_impl::make_reverse_edge_property_map(weight, G, rev_G);
+  auto distance_b_vec = std::vector<Length>(num_vertices(G_b));
+  auto distance_b =
+      make_iterator_property_map(std::begin(distance_b_vec), index_map_b);
+  kspwlo_impl::init_distance_vector(G_b, distance_b, t);
+
+  auto paths = kspwlo_impl::PathMap<Vertex>{};
+  auto paths_b = kspwlo_impl::PathMap<Vertex>{};
+  paths.insert({s, std::vector<Vertex>{s}});
+  paths_b.insert({t, std::vector<Vertex>{t}});
+  
+  // auto predecessor_b_vec = std::vector<Vertex>(num_vertices(G_b));
+  // auto predecessor_b =
+  //     make_iterator_property_map(std::begin(predecessor_b_vec), index_map_b, t);
+
+  // Init fringe structures
+  auto fringe = kspwlo_impl::Fringe<Vertex, Length>{};
+  auto fringe_b = kspwlo_impl::Fringe<Vertex, Length>{};
+  fringe.push(std::make_pair(s, 0));
+  fringe_b.push(std::make_pair(t, 0));
+
+  // Init distances to nodes seen
+  auto seen = kspwlo_impl::Seen<Vertex, Length>{};
+  auto seen_b = kspwlo_impl::Seen<Vertex, Length>{};
+  seen.insert({s, 0});
+  seen_b.insert({t, 0});
+
+  using kspwlo_impl::BiDijkStepRes;
+  using kspwlo_impl::Direction;
+
+  Length final_distance = inf;
+  auto final_path = std::vector<Vertex>{};
+  auto direction = Direction::backward;
+  while (!fringe.empty() && !fringe_b.empty()) {
+    direction = kspwlo_impl::switch_direction(direction);
+    auto result = BiDijkStepRes::next;
+
+    // Run a step
+    if (direction == Direction::forward) {
+      result = kspwlo_impl::bi_dijkstra_step(
+          G, t, paths, distance, weight, index_map, fringe, seen,
+          paths_b, distance_b, index_map_b, fringe_b, seen_b, direction,
+          final_distance, final_path);
+    } else {
+      result = kspwlo_impl::bi_dijkstra_step(
+          G_b, t, paths_b, distance_b, weight_b, index_map_b, fringe_b,
+          seen_b, paths, distance, index_map, fringe, seen, direction,
+          final_distance, final_path);
+    }
+
+    if (result == BiDijkStepRes::next) {
+      std::cout << "Next!\n";
+      continue;
+    } else if (result == BiDijkStepRes::end) {
+      for (std::size_t i = 0; i < final_path.size() - 1; ++i) {
+        predecessor[final_path[i+1]] = final_path[i];
+      }
+      std::cout << "End!\n";
+      return;
+    } else if (result == BiDijkStepRes::negative_weights) {
+      throw std::domain_error{"Contradictory paths found: negative weights? "};
+    } else {
+      break;
+    }
+  }
+
+  // Otherwise
+  throw kspwlo_impl::target_not_found{"No path found!"};
 }
 } // namespace boost
 
--- src/kspwlo/impl/bidirectional_dijkstra_impl.hpp
+++ src/kspwlo/impl/bidirectional_dijkstra_impl.hpp
@@ -7,66 +7,188 @@
 #include <boost/graph/reverse_graph.hpp>
 #include <boost/property_map/property_map.hpp>
 
+#include <iostream>
 #include <limits>
+#include <queue>
 #include <vector>
 
 namespace kspwlo_impl {
 template <typename Edge, typename Length>
 using WeightMap = std::unordered_map<Edge, Length, boost::hash<Edge>>;
 
+template <typename Vertex>
+using PathMap =
+    std::unordered_map<Vertex, std::vector<Vertex>, boost::hash<Vertex>>;
+
+template <typename Vertex, typename Length>
+using FringeElem = std::pair<Vertex, Length>;
+
+template <typename Vertex, typename Length> struct FringeComparator {
+  using Elem = FringeElem<Vertex, Length>;
+  bool operator()(const Elem &lhs, const Elem &rhs) const {
+    return lhs.second > rhs.second;
+  }
+};
+
+template <typename Vertex, typename Length>
+using Fringe = std::priority_queue<FringeElem<Vertex, Length>,
+                                   std::vector<FringeElem<Vertex, Length>>,
+                                   FringeComparator<Vertex, Length>>;
+
+template <typename Vertex, typename Length>
+using Seen = std::unordered_map<Vertex, Length, boost::hash<Vertex>>;
+
+enum class Direction { forward = 1, backward };
+constexpr Direction switch_direction(Direction prev_dir) {
+  return (prev_dir == Direction::backward) ? Direction::forward
+                                           : Direction::backward;
+}
+
+enum class BiDijkStepRes { next = 1, end, negative_weights };
+
 //===----------------------------------------------------------------------===//
 //                      Penalty algorithm classes
 //===----------------------------------------------------------------------===//
-template <typename PMap, typename Graph,
-          typename RevG = boost::reverse_graph<Graph, Graph &>>
-    class reverse_edge_property_map
-    : public boost::put_get_helper <
-      typename boost::property_traits<PMap>::reference,
-    associative_property_map<PMap> {
-  using namespace boost;
 
-public:
-  using key_type = typename property_traits<PMap>::key_type;
-  using value_type = typename property_traits<PMap>::value_type;
-  using reference = value_type &;
-  using category = lvalue_property_map_tag;
-
-  reverse_edge_property_map() : p{}, G{nullptr}, rev_g{nullptr} {}
-  reverse_edge_property_map(PMap pmap, const Graph &G, const RevG &rev_g)
-      : p{pmap}, G{std::addresof(G)}, rev_G{std::addressof(rev_g)} {}
-
-  reference operator[](const key_type &k) const {
-    auto u = source(k, rev_g);
-    auto v = target(k, rev_g);
-
-    auto[edge_in_G, is_valid] = edge(u, v, *G);
-    assert(is_valid);
-    return get(p, edge_in_G, *G);
-  }
-
-private:
-  PMap p;
-  Graph *G;
-  RevG *rev_g;
-};
-
-template <typename PMap, typename Graph>
-reverse_edge_property_map<PMap, Graph>
-make_reverse_edge_property_map(PMap pmap, const Graph &G, const RevG &rev_g) {
-  return reverse_edge_property_map(pmap, G, rev_g);
-}
-
-template <typename Length, typename Vertex, typename VertexIndexMap>
-void init_distance_vector(std::vector<Length> &distance, Vertex s,
-                          VertexIndexMap index_map) {
-  using boost::get;
+template <typename Graph, typename DistanceMap, typename Vertex>
+void init_distance_vector(Graph &G, DistanceMap distance, Vertex s) {
+  using Length = typename boost::property_traits<DistanceMap>::value_type;
   constexpr Length inf = std::numeric_limits<Length>::max();
 
-  for (auto &v : distance) {
-    distance = inf;
+  for (auto[it, end] = vertices(G); it != end; ++it) {
+    distance[*it] = inf;
   }
-  distance[get(index_map, s)] = 0;
 }
+
+template <typename Vertex>
+void merge_predecessors(Vertex w, Vertex t, PathMap<Vertex> &paths,
+                        PathMap<Vertex> &paths_b,
+                        std::vector<Vertex> &final_path) {
+  // auto v = w;
+  // while (v != t) {
+  //   auto u = predecessor_b[v];
+  //   predecessor[u] = v;
+  //   v = u;
+  // }
+  auto &path_w = paths[w];
+  auto &path_w_b = paths_b[w];
+  final_path.clear();
+  final_path.insert(final_path.end(), path_w.cbegin(), path_w.cend());
+
+  auto w_t_it = std::next(path_w_b.crbegin());
+  final_path.insert(final_path.end(), w_t_it, path_w_b.crend());
+}
+
+template <
+    typename Graph, typename DistanceMap, typename WeightMap,
+    typename VertexIndexMap, typename OtherDistanceMap, typename OtherIndexMap,
+    typename Vertex = typename boost::graph_traits<Graph>::vertex_descriptor,
+    typename Length = typename boost::property_traits<DistanceMap>::value_type>
+BiDijkStepRes
+bi_dijkstra_step(const Graph &G, Vertex t, PathMap<Vertex> &paths,
+                 DistanceMap distance, WeightMap weight,
+                 VertexIndexMap index_map, Fringe<Vertex, Length> &fringe,
+                 Seen<Vertex, Length> &seen, PathMap<Vertex> &other_paths,
+                 OtherDistanceMap other_distance, OtherIndexMap other_index_map,
+                 Fringe<Vertex, Length> &other_fringe,
+                 Seen<Vertex, Length> &other_seen, Direction direction,
+                 Length &final_distance, std::vector<Vertex> &final_path) {
+  constexpr Length inf = std::numeric_limits<Length>::max();
+  // Extract closest node to expand
+  const auto[v, dist] = fringe.top();
+  fringe.pop();
+
+  if (distance[v] != inf) {
+    // Shortest path to 'node' already found. Continue.
+    return BiDijkStepRes::next;
+  }
+
+  // Update distance
+  distance[v] = dist; // Equal to seen[index_map[v]]
+  if (other_distance[v] != inf) {
+    // If we have scanned v in both directions we are done,
+    // we have now discovered the shortest path
+
+    // Check terminating condition:
+    auto min_dist = fringe.top().second;
+    auto other_min_dist = other_fringe.top().second;
+
+    Length d_s_t = inf;
+    if (direction == Direction::forward)
+      d_s_t = final_distance;
+    else
+      d_s_t = final_distance;
+
+    auto condition = min_dist + other_min_dist > d_s_t;
+
+    std::cout << "Terminating condition: " << min_dist + other_min_dist << " > "
+              << d_s_t << ": " << std::boolalpha << condition << "\n";
+
+    if (condition) {
+      return BiDijkStepRes::end;
+    } else {
+      return BiDijkStepRes::next;
+    }
+  }
+
+  // Compute neighbor distances
+  for (auto[it, end] = out_edges(v, G); it != end; ++it) {
+    auto w = target(*it, G);
+    auto min_weight = weight[*it];
+    auto vw_length = distance[v] + min_weight;
+
+    if (distance[w] != inf) {
+      if (vw_length < distance[v]) {
+        // Throw some exception "Contradictory paths found: negative weights?"
+        return BiDijkStepRes::negative_weights;
+      }
+    } else if (auto search_w = seen.find(w);
+               search_w == std::end(seen) || vw_length < search_w->second) {
+      // Relax v-w edge
+      seen.insert_or_assign(w, vw_length);
+      fringe.push(std::make_pair(w, vw_length));
+      auto new_path = std::vector<Vertex>(paths[v]);
+      new_path.push_back(w);
+      paths.insert_or_assign(w, std::move(new_path));
+
+      // See if this path is better than the already discovered shortests path
+      if (direction == Direction::forward) {
+        auto &seen_f = seen;
+        auto &paths_f = paths;
+        auto &seen_b = other_seen;
+        auto &paths_b = other_paths;
+
+        auto search_w_f = seen_f.find(w);
+        auto search_w_b = seen_b.find(w);
+        if (search_w_f != std::end(seen_f) && search_w_b != std::end(seen_b)) {
+          auto total_distance = search_w_f->second + search_w_b->second;
+          if (final_distance > total_distance) {
+            final_distance = total_distance;
+            merge_predecessors(w, t, paths_f, paths_b, final_path);
+          }
+        }
+      } else {
+        auto seen_f = other_seen;
+        auto paths_f = other_paths;
+        auto seen_b = seen;
+        auto paths_b = paths;
+
+        auto search_w_f = seen_f.find(w);
+        auto search_w_b = seen_b.find(w);
+        if (search_w_f != std::end(seen_f) && search_w_b != std::end(seen_b)) {
+          auto total_distance = search_w_f->second + search_w_b->second;
+          if (final_distance > total_distance) {
+            final_distance = total_distance;
+            merge_predecessors(w, t, paths_f, paths_b, final_path);
+          }
+        }
+      }
+    }
+  }
+
+  return BiDijkStepRes::next;
+}
+
 } // namespace kspwlo_impl
 
 #endif
