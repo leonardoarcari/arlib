05e468a9e54909aa3e4205a54dc6b299f6013565
Leonardo Arcari <lippol94@gmail.com>
2018-05-10 12:16:18 +0200

    Bidirectional Dijkstra Performance improvement: Use more efficient data structure for shortest paths trees

--- include/kspwlo/bidirectional_dijkstra.hpp
+++ include/kspwlo/bidirectional_dijkstra.hpp
@@ -8,7 +8,9 @@
 
 #include "kspwlo/impl/bidirectional_dijkstra_impl.hpp"
 #include "kspwlo/impl/kspwlo_impl.hpp"
+#include "kspwlo/visitor.hpp"
 
+#include <deque>
 #include <limits>
 #include <vector>
 
@@ -16,32 +18,44 @@
  * @brief Algorithms and utilities for Boost::Graph
  */
 namespace boost {
+//===----------------------------------------------------------------------===//
+//                   Bidirectional Dijkstra algorithm
+//===----------------------------------------------------------------------===//
+template <typename Vertex>
+using PathMap =
+    std::unordered_map<Vertex, std::vector<Vertex>, boost::hash<Vertex>>;
+
+template <typename PredecessorMap, typename Vertex>
+void fill_predecessor(PredecessorMap predecessor,
+                      const std::deque<Vertex> &final_path) {
+  for (std::size_t i = 0; i < final_path.size() - 1; ++i) {
+    predecessor[final_path[i + 1]] = final_path[i];
+  }
+}
+
 template <
     typename Graph, typename PredecessorMap, typename DistanceMap,
-    typename WeightMap, typename VertexIndexMap, typename BackGraph,
-    typename BackWeightMap, typename BackIndexMap,
+    typename WeightMap, typename BackGraph, typename BackPredecessorMap,
+    typename BackDistanceMap, typename BackWeightMap,
+    typename BiDijkstraVisitorImpl,
     typename Vertex = typename boost::graph_traits<Graph>::vertex_descriptor>
 void bidirectional_dijkstra(const Graph &G, Vertex s, Vertex t,
                             PredecessorMap predecessor, DistanceMap distance,
-                            WeightMap weight, VertexIndexMap index_map,
-                            const BackGraph &G_b, BackWeightMap weight_b,
-                            BackIndexMap index_map_b) {
+                            WeightMap weight, const BackGraph &G_b,
+                            BackPredecessorMap predecessor_b,
+                            BackDistanceMap distance_b, BackWeightMap weight_b,
+                            BiDijkstraVisitor<BiDijkstraVisitorImpl> &visitor) {
   using namespace boost;
   using Length = typename property_traits<DistanceMap>::value_type;
   constexpr Length inf = std::numeric_limits<Length>::max();
 
   // Initialize distance structures
-  kspwlo_impl::init_distance_vector(G, distance, s);
+  kspwlo_impl::init_distance_vector(G, distance);
+  kspwlo_impl::init_distance_vector(G_b, distance_b);
 
-  auto distance_b_vec = std::vector<Length>(num_vertices(G_b));
-  auto distance_b =
-      make_iterator_property_map(std::begin(distance_b_vec), index_map_b);
-  kspwlo_impl::init_distance_vector(G_b, distance_b, t);
-
-  auto paths = kspwlo_impl::PathMap<Vertex>{};
-  auto paths_b = kspwlo_impl::PathMap<Vertex>{};
-  paths.insert({s, std::vector<Vertex>{s}});
-  paths_b.insert({t, std::vector<Vertex>{t}});
+  // Initialize shortest path trees (forward and backward)
+  predecessor[s] = s;
+  predecessor_b[t] = t;
 
   // Init fringe structures
   auto fringe = kspwlo_impl::Fringe<Vertex, Length>{};
@@ -59,7 +73,8 @@ void bidirectional_dijkstra(const Graph &G, Vertex s, Vertex t,
   using kspwlo_impl::Direction;
 
   Length final_distance = inf;
-  auto final_path = std::vector<Vertex>{};
+  bool end_phase = false;
+  auto final_path = std::deque<Vertex>{};
   auto direction = Direction::backward;
   while (!fringe.empty() && !fringe_b.empty()) {
     direction = kspwlo_impl::switch_direction(direction);
@@ -68,34 +83,81 @@ void bidirectional_dijkstra(const Graph &G, Vertex s, Vertex t,
     // Run a step
     if (direction == Direction::forward) {
       result = kspwlo_impl::bi_dijkstra_step(
-          G, t, paths, distance, weight, index_map, fringe, seen, paths_b,
-          distance_b, index_map_b, fringe_b, seen_b, direction, final_distance,
-          final_path);
+          G, s, t, predecessor, distance, weight, fringe, seen, predecessor_b,
+          distance_b, fringe_b, seen_b, direction, final_distance, final_path,
+          visitor);
     } else {
       result = kspwlo_impl::bi_dijkstra_step(
-          G_b, t, paths_b, distance_b, weight_b, index_map_b, fringe_b, seen_b,
-          paths, distance, index_map, fringe, seen, direction, final_distance,
-          final_path);
+          G_b, s, t, predecessor_b, distance_b, weight_b, fringe_b, seen_b,
+          predecessor, distance, fringe, seen, direction, final_distance,
+          final_path, visitor);
     }
 
     // Handle the outcome of this step
-    if (result == BiDijkStepRes::next) {
-      continue;
-    } else if (result == BiDijkStepRes::end) {
-      // Update predecessor map for computed shortest path
-      for (std::size_t i = 0; i < final_path.size() - 1; ++i) {
-        predecessor[final_path[i + 1]] = final_path[i];
-      }
-      return;
-    } else if (result == BiDijkStepRes::negative_weights) {
-      throw std::domain_error{"Contradictory paths found: negative weights? "};
-    } else {
+    switch (result) {
+    case BiDijkStepRes::next:
+      // Go next
       break;
+    case BiDijkStepRes::end:
+      // Fill predecessor map
+      fill_predecessor(predecessor, final_path);
+      return;
+    case BiDijkStepRes::negative_weights:
+      throw std::domain_error{"Contradictory paths found: negative weights?"};
+    default:
+      // Unexpected result
+      throw std::invalid_argument{"Unexpected result. Exiting..."};
     }
   }
 
-  // Otherwise
-  throw kspwlo_impl::target_not_found{"No path found!"};
+  if (fringe.empty() || fringe_b.empty()) {
+    // Clean exit
+    fill_predecessor(predecessor, final_path);
+    return;
+  } else {
+    // Otherwise
+    throw kspwlo_impl::target_not_found{"No path found!"};
+  }
+}
+
+template <
+    typename Graph, typename PredecessorMap, typename DistanceMap,
+    typename WeightMap, typename BackGraph, typename BackWeightMap,
+    typename BackIndexMap, typename BiDijkstraVisitorImpl,
+    typename Vertex = typename boost::graph_traits<Graph>::vertex_descriptor>
+void bidirectional_dijkstra(const Graph &G, Vertex s, Vertex t,
+                            PredecessorMap predecessor, DistanceMap distance,
+                            WeightMap weight, const BackGraph &G_b,
+                            BackWeightMap weight_b, BackIndexMap index_map_b,
+                            BiDijkstraVisitor<BiDijkstraVisitorImpl> &visitor) {
+  using namespace boost;
+  using Length = typename property_traits<DistanceMap>::value_type;
+
+  auto predecessor_b_vec =
+      std::vector<Vertex>(vertices(G_b).first, vertices(G_b).second);
+  auto predecessor_b =
+      make_iterator_property_map(std::begin(predecessor_b_vec), index_map_b);
+
+  auto distance_b_vec = std::vector<Length>(num_vertices(G_b));
+  auto distance_b =
+      make_iterator_property_map(std::begin(distance_b_vec), index_map_b);
+
+  bidirectional_dijkstra(G, s, t, predecessor, distance, weight, G_b,
+                         predecessor_b, distance_b, weight_b, visitor);
+}
+
+template <
+    typename Graph, typename PredecessorMap, typename DistanceMap,
+    typename WeightMap, typename BackGraph, typename BackWeightMap,
+    typename BackIndexMap,
+    typename Vertex = typename boost::graph_traits<Graph>::vertex_descriptor>
+void bidirectional_dijkstra(const Graph &G, Vertex s, Vertex t,
+                            PredecessorMap predecessor, DistanceMap distance,
+                            WeightMap weight, const BackGraph &G_b,
+                            BackWeightMap weight_b, BackIndexMap index_map_b) {
+  auto visitor = IdentityBiDijkstraVisitor{};
+  bidirectional_dijkstra(G, s, t, predecessor, distance, weight, G_b, weight_b,
+                         index_map_b, visitor);
 }
 } // namespace boost
 
