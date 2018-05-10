2ce32ba7faf24adbfab45b1bd9834baed9f2456f
Leonardo Arcari <lippol94@gmail.com>
2018-05-10 12:43:08 +0200

    Add script to clang-format all the things

---
+++ clang_format_all.sh
@@ -0,0 +1,10 @@
+ROOT=`git rev-parse --show-toplevel`
+
+# Clang format
+FORMAT="xargs clang-format -i --style=LLVM"
+FIND_PATTERN="-iname *.hpp -o -iname *.cpp -o -iname *.h"
+
+find $ROOT/src $FIND_PATTERN | $FORMAT
+find "$ROOT/include" $FIND_PATTERN | $FORMAT
+find "$ROOT/test" $FIND_PATTERN | $FORMAT
+find "$ROOT/dse_apps" $FIND_PATTERN | $FORMAT
