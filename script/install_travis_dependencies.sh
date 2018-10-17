f99391e8399ebb40b85710a1dfea7007c2801065
Leonardo Arcari <lippol94@gmail.com>
2018-10-17 19:36:14 +0200

    Fix doxygen bin path in PATH

--- script/install_travis_dependencies.sh
+++ script/install_travis_dependencies.sh
@@ -4,14 +4,15 @@ set -e
 mkdir -p "$HOME/opt"
 OPT="$HOME/opt"
 
-if [ ! -d "$OPT/cmake" ]; then
+if [ ! -f "$OPT/bin/cmake" ]; then
     echo "==> Installing CMake 3.12..."
     wget https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.sh
     mkdir -p "$OPT/cmake"
     sh cmake-3.12.0-Linux-x86_64.sh --prefix="$OPT/cmake" --skip-license
+    ln -s "$OPT/cmake/bin/cmake" "$OPT/bin/cmake"
 fi
 
-if [ true ]; then
+if [ ! -f "$OPT/bin/doxygen" ]; then
     echo "==> Installing Doxygen 1.8.14..."
     git clone https://github.com/doxygen/doxygen.git
     cd doxygen
