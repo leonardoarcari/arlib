a797d4a6cf6d8f4bb3e05bf5e46d60c41062407b
Leonardo Arcari <lippol94@gmail.com>
2018-10-17 17:39:30 +0200

    Download latest DOxygen release on Travis CI

---
+++ script/install_travis_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+$OPT=$HOME/opt
+
+if [ ! -d "$OPT/cmake" ]; then
+    echo "==> Installing CMake 3.12..."
+    wget https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.sh
+    mkdir -p $OPT/cmake
+    sh cmake-3.12.0-Linux-x86_64.sh --prefix=$OPT/cmake --skip-license
+fi
+
+if [ true ]; then
+    echo "==> Installing Doxygen 1.8.14..."
+    wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.14.linux.bin.tar.gz
+    mkdir -p $OPT/doxygen
+    tar vxf "doxygen-1.8.14.linux.bin.tar.gz"
+    cp -r "doxygen-1.8.14" $OPT/doxygen
+fi
