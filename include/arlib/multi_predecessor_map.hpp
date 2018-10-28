eafd2c8633e66acd049cd11daf687890f110ff28
Leonardo Arcari <lippol94@gmail.com>
2018-10-28 15:02:58 +0100

    Update documentation to most public headers

--- include/arlib/multi_predecessor_map.hpp
+++ include/arlib/multi_predecessor_map.hpp
@@ -1,3 +1,33 @@
+/**
+ * @file multi_predecessor_map.hpp
+ * @author Leonardo Arcari (leonardo1.arcari@gmail.com)
+ * @version 1.0.0
+ * @date 2018-10-28
+ *
+ * @copyright Copyright (c) 2018 Leonardo Arcari
+ *
+ * MIT Licence
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 #ifndef ALTERNATIVE_ROUTING_LIB_MULTI_PREDECESSOR_MAP_HPP
 #define ALTERNATIVE_ROUTING_LIB_MULTI_PREDECESSOR_MAP_HPP
 
@@ -7,23 +37,79 @@
 #include <memory>
 #include <unordered_map>
 
+/**
+ * An Alternative-Routing library for Boost.Graph
+ */
 namespace arlib {
+/**
+ * The multi predecessor map records the edges in the alternative paths from a
+ * pair of *source-target* vertices. Upon completion of any *alternative
+ * routing* algorithm, for any vertex `v` on any alternative path `p`,
+ * multi_predecessor_map stores the predecessor of node `v` on path `p`. If no
+ * predecessor for a node `v'` is reported, then `v'` is not part of any
+ * alternative path or it is the source node. multi_predecessor_map is a model
+ * of `Read Property Map`. The `key` type of a multi_predecessor_map is the
+ * `vertex_descriptor` of some `Graph`. Whereas the `value` type satisfies the
+ * `UnorderedAssociativeContainer` concept, where **its** key is an `int`, which
+ * is the index/number of alternative path for which it exists a predecessor of
+ * `v`, and where the value type is a `vertex descriptor` of some `Graph`.
+ *
+ * @see onepass_plus(), esx() and penalty() if you are looking for *alternative
+ *      routing* algorithms.
+ *
+ * @tparam Vertex The `vertex_descriptor`.
+ * @tparam UnorderedAssociativeContainer A model of
+ *         `UnorderedAssociativeContainer` concept. Defaulted to `std::unordered_map<int,
+ *         Vertex>`
+ */
 template <typename Vertex, typename UnorderedAssociativeContainer =
                                std::unordered_map<int, Vertex>>
 class multi_predecessor_map {
 public:
+  /**
+   * The `key` type.
+   */
   using key_type = Vertex;
+  /**
+   * The `value` type.
+   */
   using value_type =
       std::remove_cv_t<std::remove_reference_t<UnorderedAssociativeContainer>>;
+  /**
+   * Reference to value_type.
+   */
   using reference = std::add_lvalue_reference_t<value_type>;
+  /**
+   * The category of this Property Map.
+   */
   using category = boost::readable_property_map_tag;
-
+  /**
+   * Default-construct a new multi_predecessor_map object
+   */
   multi_predecessor_map()
       : _pmap{std::make_shared<std::unordered_map<
             Vertex, UnorderedAssociativeContainer, boost::hash<Vertex>>>()} {}
+  /**
+   * Copy-construct a new multi_predecessor_map object
+   *
+   * Copy operations on multi_predecessor_map are always **shallow** because
+   * Property Map requires cheap copy operation. That means that multiple copies
+   * always refer to the **same memory location**.
+   *
+   * @param other The multi_predecessor_map to copy from.
+   */
   multi_predecessor_map(multi_predecessor_map const &other)
       : _pmap{other._pmap} {}
-
+  /**
+   * Assign another multi_predecessor_map to `this`.
+   *
+   * Copy operations on multi_predecessor_map are always **shallow** because
+   * Property Map requires cheap copy operation. That means that multiple copies
+   * always refer to the **same memory location**.
+   *
+   * @param other The multi_predecessor_map to copy from.
+   * @return `this`.
+   */
   multi_predecessor_map &operator=(multi_predecessor_map const &other) {
     if (this != &other) {
       _pmap = other._pmap;
@@ -41,6 +127,27 @@ private:
       _pmap;
 };
 
+/**
+ * Lookup the value associated with `k`
+ *
+ * @tparam MultiPredecessorMap The multi predecessor map records the edges in
+ *         the alternative paths from @p s to @p t. Upon completion of the
+ *         algorithm, for any vertex `v` on any alternative path `p`, multi
+ *         predecessor map stores the predecessor of node `v` on path `p`. If no
+ *         predecessor for a node `v'` is reported, then `v'` is not part of any
+ *         alternative path or it is the source node @p s. The type of
+ *         `MultiPredecessorMap` must be a model of `Read Property Map`. The
+ *         vertex descriptor of the input graph @p G must be usable as a key
+ *         type for the multi predecessor map. Whereas the value type must
+ *         satisfy the `UnorderedAssociativeContainer` concept, where its key is
+ *         an `int`, the index/number of alternative path for which it exists a
+ *         predecessor of `v`, and where the value type is a vertex descriptor
+ *         of input graph @p G.
+ * @tparam Key Must be a valid key type for `MultiPredecessorMap`. 
+ * @param pmap The `MultiPredecessorMap` to get value from.
+ * @param k The key.
+ * @return A reference to the value associated with `k`.
+ */
 template <typename MultiPredecessorMap, typename Key>
 typename MultiPredecessorMap::reference get(MultiPredecessorMap &pmap,
                                             Key const &k) {
--- include/arlib/routing_kernels/visitor.hpp
+++ include/arlib/routing_kernels/visitor.hpp
@@ -1,3 +1,33 @@
+/**
+ * @file visitor.hpp
+ * @author Leonardo Arcari (leonardo1.arcari@gmail.com)
+ * @version 1.0.0
+ * @date 2018-10-28
+ *
+ * @copyright Copyright (c) 2018 Leonardo Arcari
+ *
+ * MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
 #ifndef ARLIB_VISITOR_HPP
 #define ARLIB_VISITOR_HPP
 
@@ -7,21 +37,63 @@
 #include <unordered_set>
 
 /**
- * @brief Algorithms and utilities for Boost::Graph
+ * An Alternative-Routing library for Boost.Graph
  */
 namespace arlib {
 //===----------------------------------------------------------------------===//
 //                    Bidirectional Dijkstra visitor
 //===----------------------------------------------------------------------===//
+/**
+ * CRTP interface class for bidirectional_dijkstra() visitor.
+ *
+ * A BiDijkstraVisitor is passed to bidirectional_dijkstra() in order to
+ * customize the algorithm behavior with respect to
+ *  - terminating_condition(): when a vertex is found both from source and
+ * target searches, bidirectional_dijkstra() asks the visitor whether to end the
+ *    search or keep going.
+ *  - expand_vertex(): given a vertex v, its distance from one root and the
+ *    distance-lower-bound from the other and the current minimum st-distance,
+ *    the visitor should return whether to expand the search in the direction of
+ *    v or not.
+ *
+ * @tparam Derived The derived class implementing BiDijkstraVisitor interface
+ */
 template <typename Derived> class BiDijkstraVisitor {
 public:
+  /**
+   * Whether or not bidirectional_dijkstra() should stop. It is invoked when a
+   * vertex v is found both from source search and target search.
+   *
+   * @tparam Length The weight value type.
+   * @param min_distance The minimum distance to any node in current direction
+   *        fringe.
+   * @param other_min_distance The minimum distance to any node in the other
+   *        direction fringe.
+   * @param st_distance Current minimum source-target distance.
+   * @return true if bidirectional_dijkstra() should stop.
+   * @return false otherwise.
+   */
   template <typename Length>
   bool terminating_condition(Length min_distance, Length other_min_distance,
                              Length st_distance) {
     return static_cast<Derived *>(this)->terminating_condition(
         min_distance, other_min_distance, st_distance);
   }
-
+  /**
+   * Whether or not to bidirectional_dijkstra() should expand the search through
+   * the input Vertex v.
+   *
+   * @tparam Vertex The vertex_descriptor.
+   * @tparam Length The weight value type.
+   * @param v The candidate Vertex to expand.
+   * @param v_distance The Vertex distance according to current-direction
+   *        search.
+   * @param lower_bound_v The Vertex distance-heuristic according to
+   *        opposite-direction search.
+   * @param st_distance Current minimum source-target distance.
+   * @return true if bidirectional_dijkstra() should expand vertex @p v
+   * @return false otherwise.
+   */
   template <typename Vertex, typename Length>
   bool expand_vertex(Vertex v, Length v_distance, Length lower_bound_v,
                      Length st_distance) {
@@ -30,30 +102,97 @@ public:
   }
 };
 
+/**
+ * BiDijkstraVisitor implementing vanilla Bidirectional Dijkstra behavior as
+ * described in Nicholson's paper (1966):
+ * https://academic.oup.com/comjnl/article/9/3/275/406281
+ */
 class IdentityBiDijkstraVisitor
     : public BiDijkstraVisitor<IdentityBiDijkstraVisitor> {
 public:
+  /**
+   * Vanilla Bidirectional Dijkstra stopping condition.
+   *
+   * @tparam Length The weight value type.
+   * @param min_distance The minimum distance to any node in current direction
+   *        fringe.
+   * @param other_min_distance The minimum distance to any node in the other
+   *        direction fringe.
+   * @param st_distance Current minimum source-target distance.
+   * @return true if @p min_distance + @p other_min_distance > @p st_distance.
+   * @return false otherwise.
+   */
   template <typename Length>
   bool terminating_condition(Length min_distance, Length other_min_distance,
                              Length st_distance) {
     return (min_distance + other_min_distance) > st_distance;
   }
-
+  /**
+   * Always-expand policy.
+   *
+   * @tparam Vertex The vertex_descriptor
+   * @tparam Length The weight value type.
+   * @return true always.
+   */
   template <typename Vertex, typename Length>
   bool expand_vertex(Vertex, Length, Length, Length) {
     return true;
   }
 };
 
+/**
+ * BiDijkstraVisitor implementing Uninformed Bidirectional Pruner as described
+ * in
+ *
+ * A. Paraskevopoulos, C. Zaroliagis, Improved alternative route
+ * planning,198in: OASIcs-OpenAccess Series in Informatics, Vol. 33, Schloss
+ * Dagstuhl-199Leibniz-Zentrum fuer Informatik, 2013
+ * http://drops.dagstuhl.de/opus/volltexte/2013/4248/
+ *
+ * The purpose of this visitor is to explore the whole graph an keep track of
+ * all those vertices that should be pruned.
+ * Let v be a Vertex, let tau be the pruning factor, v_distance be the distance
+ * of v from the root of current search, other_min_distance be
+ * distance-heuristic of v from the opposite search-root and st_distance be the
+ * current minimum distance between source and target nodes. Then v should be
+ * pruned if:
+ * v_distance + lower_bound_v > tau * st_distance
+ *
+ * @see uninformed_bidirectional_pruner()
+ *
+ * @tparam Vertex The vertex_descriptor
+ */
 template <typename Vertex>
 class UninformedBiPrunerVisitor
     : public BiDijkstraVisitor<UninformedBiPrunerVisitor<Vertex>> {
 public:
+  /**
+   * A set of Vertex of the graph
+   */
   using VertexSet = std::unordered_set<Vertex, boost::hash<Vertex>>;
+  /**
+   * The VertexSet const-iterator type
+   */
   using const_iterator = typename VertexSet::const_iterator;
+  /**
+   * Construct a new UninformedBiPrunerVisitor object with a pruning factor of
+   * @p tau.
+   *
+   * @param tau The pruning factor.
+   */
   explicit UninformedBiPrunerVisitor(double tau)
       : tau{tau}, is_pruning_phase{false} {}
-
+  /**
+   * Always keep searching.
+   *
+   * @tparam Length The weight value type.
+   * @param min_distance The minimum distance to any node in current direction
+   *        fringe.
+   * @param other_min_distance The minimum distance to any node in the other
+   *        direction fringe.
+   * @param st_distance Current minimum source-target distance.
+   * @return false always.
+   */
   template <typename Length>
   bool terminating_condition(Length min_distance, Length other_min_distance,
                              Length st_distance) {
@@ -62,7 +201,22 @@ public:
     }
     return false;
   }
-
+  /**
+   * If the shortest-path from source to target hasn't been found yet, then
+   * always expand. Otherwise, expand only if
+   * @p v_distance + @p lower_bound_v <= tau * @p st_distance
+   *
+   * @tparam Length The weight value type.
+   * @param v The candidate Vertex to expand.
+   * @param v_distance The Vertex distance according to current-direction
+   *        search.
+   * @param lower_bound_v The Vertex distance-heuristic according to
+   *        opposite-direction search.
+   * @param st_distance Current minimum source-target distance.
+   * @return true If the shortest-path from source to target hasn't been found
+   * yet or if @p v_distance + @p lower_bound_v <= tau * @p st_distance
+   * @return false otherwise.
+   */
   template <typename Length>
   bool expand_vertex(Vertex v, Length v_distance, Length lower_bound_v,
                      Length st_distance) {
@@ -76,11 +230,17 @@ public:
     } else
       return true;
   }
-
+  /**
+   * @return a pair of const-iterators to begin and end of the pruned VertexSet.
+   */
   std::pair<const_iterator, const_iterator> get_pruned_vertices() const {
     return {pruned_vertices.cbegin(), pruned_vertices.cend()};
   }
-
+  /**
+   * @param v The vertex to test
+   * @return true if @p v should be pruned.
+   * @return false otherwise.
+   */
   bool is_pruned(const Vertex &v) const {
     return (pruned_vertices.find(v) != std::end(pruned_vertices));
   }
