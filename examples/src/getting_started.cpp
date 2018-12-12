3e955a9d604a9ebbda984f8bc3cf0cd5e601ef07
Leonardo Arcari <lippol94@gmail.com>
2018-12-12 15:30:39 +0100

    Fix a compilation error in Getting Started example

--- examples/src/getting_started.cpp
+++ examples/src/getting_started.cpp
@@ -1,10 +1,11 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
 
-#include <arlib/path.hpp>
-#include <arlib/graph_utils.hpp>
 #include <arlib/multi_predecessor_map.hpp>
 #include <arlib/onepass_plus.hpp>
+#include <arlib/path.hpp>
+
+#include <arlib/graph_utils.hpp>
 
 #include <iostream>
 #include <string>
