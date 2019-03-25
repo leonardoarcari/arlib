11ed6895f5e71eb7a3491e2cb8586e733c78ef8a
Leonardo Arcari <lippol94@gmail.com>
2019-03-25 12:18:55 +0100

    First attempt to fix Travis CI error

--- script/install_travis_dependencies.sh
+++ script/install_travis_dependencies.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 set -e
-mkdir -p "$HOME/opt"
-OPT="$HOME/opt"
+# mkdir -p "$HOME/opt"
+# OPT="$HOME/opt"
 
-if [ ! -f "$OPT/cmake/bin/cmake" ]; then
-    echo "==> Installing CMake 3.12..."
-    wget https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.sh
-    mkdir -p "$OPT/cmake"
-    sh cmake-3.12.0-Linux-x86_64.sh --prefix="$OPT/cmake" --skip-license    
-fi
+# if [ ! -f "$OPT/cmake/bin/cmake" ]; then
+#     echo "==> Installing CMake 3.12..."
+#     wget https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.sh
+#     mkdir -p "$OPT/cmake"
+#     sh cmake-3.12.0-Linux-x86_64.sh --prefix="$OPT/cmake" --skip-license    
+# fi
 
-# In any case, create a CMake link to $OPT/bin
-ln -s "$OPT/cmake/bin/cmake" "$OPT/bin/cmake"
+# # In any case, create a CMake link to $OPT/bin
+# ln -s "$OPT/cmake/bin/cmake" "$OPT/bin/cmake"
 
-if [ ! -f "$OPT/bin/doxygen" ]; then
-    echo "==> Installing Doxygen 1.8.14..."
-    git clone https://github.com/doxygen/doxygen.git
-    cd doxygen
-    mkdir build
-    cd build
-    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_INSTALL_PREFIX="$OPT" ..
-    make -j2
-    make install
-fi
+# if [ ! -f "$OPT/bin/doxygen" ]; then
+#     echo "==> Installing Doxygen 1.8.14..."
+#     git clone https://github.com/doxygen/doxygen.git
+#     cd doxygen
+#     mkdir build
+#     cd build
+#     cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_INSTALL_PREFIX="$OPT" ..
+#     make -j2
+#     make install
+# fi
