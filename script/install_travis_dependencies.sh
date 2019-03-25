452a17acb9f3ee76cf17beadef6854b8f5186411
Leonardo Arcari <lippol94@gmail.com>
2019-03-25 12:30:10 +0100

    Restore compiled-doxygen into Travis

--- script/install_travis_dependencies.sh
+++ script/install_travis_dependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 set -e
-# mkdir -p "$HOME/opt"
-# OPT="$HOME/opt"
+mkdir -p "$HOME/opt"
+OPT="$HOME/opt"
 
 # if [ ! -f "$OPT/cmake/bin/cmake" ]; then
 #     echo "==> Installing CMake 3.12..."
@@ -14,13 +14,13 @@ set -e
 # # In any case, create a CMake link to $OPT/bin
 # ln -s "$OPT/cmake/bin/cmake" "$OPT/bin/cmake"
 
-# if [ ! -f "$OPT/bin/doxygen" ]; then
-#     echo "==> Installing Doxygen 1.8.14..."
-#     git clone https://github.com/doxygen/doxygen.git
-#     cd doxygen
-#     mkdir build
-#     cd build
-#     cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_INSTALL_PREFIX="$OPT" ..
-#     make -j2
-#     make install
-# fi
+if [ ! -f "$OPT/bin/doxygen" ]; then
+    echo "==> Installing Doxygen 1.8.14..."
+    git clone https://github.com/doxygen/doxygen.git
+    cd doxygen
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-8 -DCMAKE_INSTALL_PREFIX="$OPT" ..
+    make -j2
+    make install
+fi
